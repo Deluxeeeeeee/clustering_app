e765c20b8d3dc033f095f5c039df2773bfb54ec6
Deluxeeeeeee <165366945+Deluxeeeeeee@users.noreply.github.com>
2025-04-20 16:49:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open ('kmeans_model.pkl', 'rb') as f:
 st.title("k-Means Clustering Visualizer by Thuta Zaw")
 
 #set the page config
-st.set_page_config(page_title="k-Means Clustering", layout="centered")
+st.set_page_config(page_title = "k-Means Clustering", layout="centered")
 
 #Load dataset
 X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
